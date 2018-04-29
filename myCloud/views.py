8bcbfde4379d2686141d6d655f0a3725abbd43c8
gaox <zhejiangblue@hotmail.com>
2018-04-29 13:56:15 +0800

    Upload File Finished

--- myCloud/views.py
+++ myCloud/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
 
+from .forms import FileModelForm
+from .models import FileModel
 # Create your views here.
+class IndexView(View):
+    def get(self,request):
+        context={}
+        return render(request, 'myCloud/index.html', context)
+
+
+# # 采用普通表单方式
+# class SaveFileView(View):
+#     def post(self,request):
+#         data = FileModelForm(request.POST, request.FILES)
+#         if data.is_valid():
+#             form = FileModel()
+#             form.fname = data.cleaned_data["fname"]
+#             form.file = data.cleaned_data['file']
+#             form.save()
+#             return HttpResponse("上传成功")
+#         else:
+#             return HttpResponse("上传失败")
+
+# 采用模型表单方式
+class SaveFileView(View):
+    def post(self,request):
+        form = FileModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("上传成功")
+        else:
+            return HttpResponse("上传失败")
+
+
+
+
+
+
